fe9501a1847f6c1328808fdacd103262c9d1ad02
Ahmeed12 <91153281+Ahmeed12@users.noreply.github.com>
2021-09-24 17:03:30 +0300

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+//name:Ahmed Atiah Almalki
+//ID:442010256
+public class Main {
+  public static void main(String[] args) {
+  
+  Scanner input= new Scanner(System.in);
+  float a;
+  float b;
+  System.out.println("please inter 2 Decimalnumber" );
+  a=input.nextfloat();
+  b=input.nextfloat();
+  System.out.println(a+b);
+  System.out.println(a-b);
+  System.out.println(a*b);
+  System.out.println(a/b);
+  System.out.println(a%b);
+  System.out.println(a==b);  
+  System.out.println(a!=b);
+  System.out.println(a<=b);
+  System.out.println(a>=b);
+  System.out.println(a<b);
+  System.out.println(a>b);
+ 
+  
+  
+  }
+  }
