679b1527bf5f31cc8879881958c3ee4bc0b9a6a7
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-29 23:57:46 +0300

    fix 3 issues

--- Assignment.java
+++ Assignment.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
+
 //name:Ahmed Atiah Almalki
 //ID:442010256
-public class Main {
-  public static void main(String[] args) {
-  
-  Scanner input= new Scanner(System.in);
-  float a;
-  float b;
-  System.out.println("please inter 2 Decimalnumber" );
-  a=input.nextfloat();
-  b=input.nextfloat();
-  System.out.println(a+b);
-  System.out.println(a-b);
-  System.out.println(a*b);
-  System.out.println(a/b);
-  System.out.println(a%b);
-  System.out.println(a==b);  
-  System.out.println(a!=b);
-  System.out.println(a<=b);
-  System.out.println(a>=b);
-  System.out.println(a<b);
-  System.out.println(a>b);
- 
-  
-  
-  }
-  }
+//your grade is 3.5/5
+//@ line 5: change the Main to match the file name like Assignment
+public class Assignment {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        float a;
+        float b;
+        System.out.println("please inter 2 Decimalnumber");
+        //@ make sure that the function is nextFloat
+        a = input.nextfloat();
+        //@ make sure that the function is nextFloat
+        b = input.nextfloat();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+        System.out.println(a < b);
+        System.out.println(a > b);
+
+
+    }
+}
