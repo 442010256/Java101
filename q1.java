ce1328dc29723a10bc08113ae51a18ebda331dd4
Java101 <91153281+442010256@users.noreply.github.com>
2021-10-12 11:08:38 +0300

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q1
+{
+	public static void main(String[] args) {
+	
+    System.out.println("enter a number");	
+	
+    Scanner scan=new Scanner(System.in);	
+	int var=scan.nextInt();	
+	if(var %2==0)	
+		
+		System.out.println("even");	
+		
+	else	
+	System.out.println("odd");	
+		
+
+	}
+}
