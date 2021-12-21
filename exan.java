9c72be7214687ec2916ac6a390380ce23dfd9e79
Java101 <91153281+442010256@users.noreply.github.com>
2021-12-21 10:35:59 +0300

    Create exan.java

---
+++ exan.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+	
+	  int counter = 100; 
+	    
+	   while( counter>=-100 ) {
+	    
+	    if(counter%2==0){
+	    
+	   System.out.println(counter+"%2 is even");
+	     }else {
+	    System.out.println(counter+"%2 is odd"); 
+	    }
+	    --counter;
+	}
+}
+}
